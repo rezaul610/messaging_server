394a62a51cc785be16ac63b9671a44c5887bc5d2
Nano <nano@gmail.com>
2025-12-29 11:08:50 +0000

    database.js database information facing from .env

--- config/database.js
+++ config/database.js
@@ -1,20 +1,16 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize("chat_db", "root", "", {
-    host: "localhost",
-    dialect: "mysql",
-    logging: false,
-});
-
-(async () => {
-    try {
-        await sequelize.authenticate()
-            .then(() => console.log('✅ MySQL connected!'))
-            .catch(err => console.error('❌ Connection error:', err));
-        await sequelize.sync({ alter: true }); // sync models to DB
-    } catch (error) {
-        console.error("❌ MySQL connection failed:", error.message);
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "mysql",
+        logging: false,
     }
-})();
+);
 
 module.exports = sequelize;
