7168838cd745ba2d6ce4f6c1dfd4c625dc4ced51
Rezaul Karim <rezaul.karim.cse.610@gmail.com>
2026-01-26 09:16:18 +0600

    update push messaging server

--- middleware/notify.js
+++ middleware/notify.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../config/firebase.serviceAccount.json");
+const serviceAccount = require("../middleware/firebaseServiceAccount.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
