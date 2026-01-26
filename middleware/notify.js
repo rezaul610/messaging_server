2a5d1e9ab49e625bce6060541d6885c1bf085fde
Rezaul Karim <rezaul.karim.cse.610@gmail.com>
2026-01-26 11:20:56 +0600

    Clean commit (removed secrets)

--- middleware/notify.js
+++ middleware/notify.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../middleware/firebaseServiceAccount.json");
+const serviceAccount = require("../config/firebase.serviceAccount.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
